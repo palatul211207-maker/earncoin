62edfca9c09e8b3d5be045c964e7d4b65d389f07
palatul211207-maker <palatul211207@gmail.com>
2026-02-02 09:37:03 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-service-worker.js
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("earncoin").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
